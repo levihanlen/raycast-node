4660bfa33b9c589a56e67493e08f01a8566957fb
Levi Hanlen <134863518+levihanlen@users.noreply.github.com>
2024-12-24 16:08:33 -0600

    feat: improve that jawn

--- src/main.ts
+++ src/main.ts
@@ -19,20 +19,20 @@ const ctx = canvas.getContext("2d")!;
 const user: User = {
   x: 4,
   y: 4,
-  angle: 90,
-  fov: 10,
+  angle: 180,
+  fov: 40,
   moveSpeed: 0.05, // Movement speed
   rotSpeed: 3, // Rotation speed
 };
 
-const totalRays = 200;
+const totalRays = 500;
 
 const using2d = false;
 
 const map = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+  [1, 0, 1, 0, 2, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
   [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
   [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -86,12 +86,20 @@ function gameLoop() {
 
   handleInput();
 
+  drawSkybox();
   castRays();
   if (using2d) {
     draw2d();
   }
 
   requestAnimationFrame(gameLoop);
+}
+
+function drawSkybox() {
+  ctx.fillStyle = `hsl(208, 39.90%, 70.00%)`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height / 2);
+  ctx.fillStyle = `hsl(19, 39.90%, 70.00%)`;
+  ctx.fillRect(0, canvas.height / 2, canvas.width, canvas.height / 2);
 }
 
 function handleInput() {
@@ -128,10 +136,22 @@ function moveUser(amount: number) {
 
 function castRays() {
   const maxDist = 1000;
-  const moveDist = 0.001;
+  const moveDist = 0.01;
   for (let i = 0; i < totalRays; i++) {
-    const rayAngle = (i - user.fov + user.angle) % 360;
+    //  i = 0, then -fov
+    // i = totalrays, then fov
+
+    const rayAngle =
+      (i * (user.fov / totalRays) - user.fov / 2 + user.angle) % 360;
     const radians = (rayAngle / 180) * Math.PI;
+    /*
+    if (i === 0) {
+      console.log(rayAngle);
+    }
+    if (i === totalRays - 1) {
+      console.log(rayAngle);
+    }
+    */
 
     const xMove = Math.cos(radians) * moveDist;
     const yMove = Math.sin(radians) * moveDist;
@@ -151,18 +171,25 @@ function castRays() {
         dist += moveDist;
       }
     }
-    drawRay(i, dist);
+    const wallType = findWallType(x, y);
+    drawRay(i, dist, wallType);
     drawRay2d(x, y);
   }
 }
 
-function drawRay(i: number, dist: number) {
-  const bar = canvas.width / user.fov;
+function drawRay(i: number, dist: number, wallType: number = 1) {
+  const bar = canvas.width / totalRays;
   const startX = (i / totalRays) * canvas.width;
 
-  ctx.fillStyle = `hsl(10, 0%, ${dist * 5}%)`;
-  const height = ((1 / dist) * canvas.height) / 2;
+  // const hue = Math.random() * 360;
+  const hue = wallType * 20;
+  ctx.fillStyle = `hsl(${hue}, 50%, ${dist * 5 + 20}%)`;
+  const height = ((1 / dist) * canvas.height) / 1;
   ctx.fillRect(startX, canvas.height / 2 - height / 2, bar, height);
+}
+
+function findWallType(x: number, y: number) {
+  return roundToGrid(x, y);
 }
 
 function drawRay2d(x: number, y: number) {
@@ -178,16 +205,19 @@ function drawRay2d(x: number, y: number) {
   ctx.stroke();
 }
 
-function findColliding(x: number, y: number) {
+function roundToGrid(x: number, y: number) {
   const { x: floorX, y: floorY } = keepWithin(Math.floor(x), Math.floor(y));
-  const thing = map[floorY][floorX];
+  return map[floorY][floorX];
+}
+function findColliding(x: number, y: number) {
+  const thing = roundToGrid(x, y);
 
   //  const { x: ceilX, y: ceilY } = keepWithin(Math.ceil(x), Math.ceil(y));
   // console.log("ciel", ceilX, ceilY);
   // const thing2 = map[ceilY][ceilX];
   // debugger;
   // console.log(thing, thing2);
-  if (thing === 1) {
+  if (thing !== 0) {
     return true;
   } else {
     return false;
